1f0f70e88038df6d786671d5ec5ff9439dbb49cc
Chelsea David <cryan17@cnm.edu>
2018-07-31 13:57:02 -0600

    updated content strategy

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 2</title>
+	</head>
+
+	<h1>Personal Website Wireframe and Content Strategy</h1>
+
+	<h3>Content Strategy</h3>
+	<!-- Content Strategy -->
+	<p>For my content strategy my site will be simple. less is less. I plan to have an image similar to the one<br>
+		uploaded as my "welcome image".   All information sections will be in plain white background with standard text<br>
+		and between information sections I will have different colored background sections that will divide each group<br>
+		and keep with color scheme of welcome image,<br>
+		The languages and skills section will have two features. both icons representing the languages i have learned and<br>
+		a detailed section listing professional skills personality traits that help market me to a possible future employer.<br>
+		The contact me section will be colored in keeping with the color scheme. though being that the section will be entirely<br>
+		color, it will need to be subtle. And finally i would like to have a constant menu header AND contact icons footer<br>
+		as to always have information available anywhere in the page.
+	</p>
+
+<!-- Example picture of wireframe -->
+	<h3>Wireframe</h3>
+
+	<img src="../images/pwpwireframe.png">
+
+</html> 
