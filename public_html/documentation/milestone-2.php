3889f64a8558019b0bfbb43883005325754cb3f8
Chelsea David <cryan17@cnm.edu>
2018-07-31 16:59:27 -0600

    updated content strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -10,20 +10,31 @@
 
 	<h3>Content Strategy</h3>
 	<!-- Content Strategy -->
-	<p>For my content strategy my site will be simple. less is less. I plan to have an image similar to the one<br>
-		uploaded as my "welcome image".   All information sections will be in plain white background with standard text<br>
-		and between information sections I will have different colored background sections that will divide each group<br>
-		and keep with color scheme of welcome image,<br>
-		The languages and skills section will have two features. both icons representing the languages i have learned and<br>
-		a detailed section listing professional skills personality traits that help market me to a possible future employer.<br>
-		The contact me section will be colored in keeping with the color scheme. though being that the section will be entirely<br>
-		color, it will need to be subtle. And finally i would like to have a constant menu header AND contact icons footer<br>
-		as to always have information available anywhere in the page.
-	</p>
+	<p>For my content strategy my site will be simple. less is less.</p>
+	<!-- Welcome Section -->
+		<h4>Welcome section</h4>
+			I plan to have an image similar to the one uploaded as my "welcome image".</li>
+	<!-- About Me -->
+		<h4>About me</h4>
+		<p>All information and about me section will be in plain white background with standard text</p>
+		<!-- Background color scheme -->
+		<p>Between information sections I will have different colored background sections that will divide each group
+			and keep with color scheme of welcome image</p>
+		<!-- Languages and skills -->
+		<h4>Languages and Skill</h4>
+		<p>The languages and skills section will have two features. Both icons representing the languages I have learned<br>
+			as well as a detailed section listing professional skills, and personality traits that help market me to a possible future employer.</p>>
+		<!-- Contact Me -->
+		<h4>Contact Me</h4>
+		<p>The contact me section will be colored in keeping with the color scheme. though being that the section will be entirely
+			color, it will need to be subtle.</p>
+		<!-- Headers & Footer -->
+		<p>And finally I would like to have a constant menu header AND contact icons footer<br>
+			as to always have information available anywhere in the page.</p>
 
-<!-- Example picture of wireframe -->
+<!-- Example picture of wire-frame -->
 	<h3>Wireframe</h3>
 
-	<img src="../images/pwpwireframe.png">
+	<img src="../documentation/images/pwpwireframe.png" alt="Wire frame layout">
 
 </html> 
